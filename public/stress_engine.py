8c45ae900018d83d689639b5929223af2f1b3bb4
lsec-code <linuxsec11@gmail.com>
2026-01-01 08:25:44 +0700

    Update stress_engine.py

--- public/stress_engine.py
+++ public/stress_engine.py
@@ -58,7 +58,15 @@ def attack(target_url, end_time, thread_id, port, mode):
             curr_url = target_url
             headers = {
                 'User-Agent': random.choice(USER_AGENTS),
-                'Accept': '*/*'
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Connection': 'keep-alive',
+                'Upgrade-Insecure-Requests': '1',
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'none',
+                'Sec-Fetch-User': '?1'
             }
 
             # MODE LOGIC (Simplified for brevity in diff)
